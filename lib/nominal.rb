2f3fbf27787c90560e0d78c366c1fe53d4942583
Wirwing <irving.cf@gmail.com>
2014-11-19 19:07:42 -0600

    Se agrega api key al objeto Nominal.

--- lib/nominal.rb
+++ lib/nominal.rb
@@ -1,19 +1,33 @@
-require 'json'
+require "json"
 require "nominal/version"
 
 module Nominal
+
   @api_base = 'https://api.nominal.mx'
   @api_version = '0.0.1'
+
   def self.api_base
     @api_base
   end
+
   def self.api_base=(api_base)
     @api_base = api_base
   end
+
   def self.api_version
     @api_version
   end
+
   def self.api_version=(api_version)
     @api_version = api_version
   end
+
+  def self.api_key
+    @api_key
+  end
+
+  def self.api_key=(api_key)
+    @api_key = api_key
+  end
+
 end
